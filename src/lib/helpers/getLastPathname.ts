bf3c74a89c60296c2aa6e646f68a37bf9a9ea7c9
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-05-14 06:25:21 +0700

    fix: remove darkmode components because it's unnecessary for now

--- src/lib/helpers/getLastPathname.ts
+++ src/lib/helpers/getLastPathname.ts
@@ -5,8 +5,9 @@
  * @returns {string}
  */
 export function getLastPathname(pathname: string) {
-  const lastPathname: string = pathname
-    .split("/")[2]
+  const pathnameSplit = pathname.split("/");
+
+  const lastPathname: string = pathnameSplit[pathnameSplit.length - 1]
     .split("-")
     .map((item) => item[0].toUpperCase() + item.substring(1))
     .join(" ");
